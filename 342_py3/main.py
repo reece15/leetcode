2179c2b8eb786bd0774ada08244f95220b421a93
qiang_ge@outlook.com <qiang_ge@outlook.com>
2018-10-22 23:04:06 +0800

    add 342

---
+++ 342_py3/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 1:
+            return True
+
+        data = bin(num)[2:]
+
+        return data.startswith("100") and (data[3:].count("0") % 2 == 0 and data[3:].count("1") == 0)
+
+
+    
